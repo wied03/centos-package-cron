17dfab31f34430cdd463c19ae73c66b091717b81
Brady Wied <brady@wied.us>
2014-09-26 14:20:18 -0500

    Handle lower versions, that test now passes

--- test/test_package_checker.py
+++ test/test_package_checker.py
@@ -74,6 +74,27 @@ class PackageCheckerTest(unittest.TestCase):
 
 		# assert
 		self.assertEquals(result, [])
+		
+	def testFindAdvisoriesOnInstalledPackagesInstalledButLowerVersion(self):
+		# arrange
+		errata = Mock()
+		errata.get_errata = Mock(return_value=[
+		errata_fetcher.ErrataItem('adv id', ErrataType.SecurityAdvisory,ErrataSeverity.Important, ['x86_64'], ['7'], [{'name': 'xen-libs','version':'3.0.3', 'release':'135.el5_8.2', 'arch':'x86_64'}])
+		])
+		pkg = Mock()
+		pkg.fetch_installed_packages = Mock(return_value=[
+		package_fetcher.Package('xen-libs','3.0.2', '135.el5_8.2', 'x86_64'),
+		package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64')
+		])
+		os_fetcher = Mock()
+		os_fetcher.get_top_level_version = Mock(return_value='7')
+		checker = package_checker.PackageChecker(errata,pkg,os_fetcher)
+
+		# act
+		result = checker.findAdvisoriesOnInstalledPackages()
+
+		# assert
+		self.assertNotEquals(result, [])
 
 	def testFindAdvisoriesOnInstalledPackagesInstalledButNewerRelease(self):
 		# arrange
