d1cb69c93a8849d0a7b13cf04d5248cdc37413be
Brady Wied <brady@wied.us>
2014-09-27 17:52:48 -0500

    Command line/mail interface now works

--- centos_package_cron/command_line.py
+++ centos_package_cron/command_line.py
@@ -3,8 +3,12 @@ import package_fetcher
 import errata_fetcher
 import os_version_fetcher
 import mockable_execute
+import argparse
+import socket
 
-def main():
+__VERSION__ = '1.0'
+
+def handle_args(sys_args):
 	executor = mockable_execute.MockableExecute()
 	pkg_fetcher = package_fetcher.PackageFetcher(package_fetcher.ChangeLogParser(),executor)
 	checker = package_checker.PackageChecker(errata_fetcher.ErrataFetcher(),pkg_fetcher,os_version_fetcher.OsVersionFetcher())
@@ -31,7 +35,36 @@ def main():
 			changelog_entry = next(cl for cl in changelogs if cl['name'] == update.name)			
 			email_content += "Updated package %s version %s release %s, change log:\n%s\n\n" % (update.name, update.version, update.release, changelog_entry['changelog'])
 		
-		executor.run_command(['/usr/bin/mail', '-s theSubject', '-r joefrom@rcn.com', 'vagrant'], email_content)
+		executor.run_command(['/usr/bin/mail',
+							  '-s %s' % (sys_args.email_subject),
+							  '-r %s' % (sys_args.email_from),
+							  sys_args.email_to],
+							 email_content)
 
 if __name__ == "__main__" :
-    main()
+	parser = argparse.ArgumentParser(description="Emails administrators with CentOS security updates and changelogs of non-security updates. Version %s" % __VERSION__)
+	
+	parser.add_argument('-e', '--email_to',
+	type=str,
+	required=True,
+	help='Email following user with the output.'
+	'Could be `centos_package_cron -e johndoe@mail.com`. '
+	"Uses system's `mail` to send emails.")
+	
+	default_from = "CentOS Update Check on %s <noreply@centos.org>" %(socket.gethostname())
+	parser.add_argument('-f', '--email_from',
+	type=str,
+	default=default_from,
+	help='Email from this user'
+	'Could be `centos_package_cron -f johndoe@mail.com`. '
+	"Uses system's `mail` to send emails  Will use '%s' by default." % (default_from))
+	
+	default_subject= "CentOS Update Check on %s" %(socket.gethostname())
+	parser.add_argument('-s', '--email_subject',
+	type=str,
+	default=default_subject,
+	help='Email using this subject'
+	'Could be `centos_package_cron -s "the test subject"`. '
+	"Uses system's `mail` to send emails with this subject  Will use '%s' by default." % (default_subject))
+	
+	handle_args(parser.parse_args())
