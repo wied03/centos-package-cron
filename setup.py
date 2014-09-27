284ac6c9af647aa62c9d61382f4585e42f02fc79
Brady Wied <brady@bswtechconsulting.com>
2014-09-27 12:48:27 -0500

    got further progress

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import setup, find_packages	
 
 setup(name='centos-package-cron',
       version='1.0',
@@ -8,6 +9,6 @@ setup(name='centos-package-cron',
       author='Brady Wied',
       author_email='support@bswtechconsulting.com',
       url='https://github.com/wied03/centos-package-cron',
-      packages=['centos-package-cron'],
+      packages=find_packages(),
       test_suite="tests",
      )
