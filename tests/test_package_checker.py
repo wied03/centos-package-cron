c7f2d1d046a246f8add5ccc434a9ce999e2b980b
Brady Wied <brady@wied.us>
2014-10-23 12:17:28 -0500

    Use import/shorter syntax

--- tests/test_package_checker.py
+++ tests/test_package_checker.py
@@ -8,6 +8,7 @@ from centos_package_cron import errata_fetcher
 from centos_package_cron.errata_fetcher import ErrataType
 from centos_package_cron.errata_fetcher import ErrataSeverity
 from mock import Mock
+from centos_package_cron.package import Package
 
 class PackageCheckerTest(unittest.TestCase):
     def testSameVersionOfAnotherPackageInstalled(self):
@@ -18,7 +19,7 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-                package_fetcher.Package('libgcrypt', '1.5.3', '4.el7', 'x86_64', 'updates')
+                Package('libgcrypt', '1.5.3', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -38,8 +39,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('bash','1.0', '4.el7', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('bash','1.0', '4.el7', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -59,8 +60,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.3', '135.el5_8.2', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.3', '135.el5_8.2', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -80,8 +81,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.4', '135.el5_8.2', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.4', '135.el5_8.2', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -110,7 +111,7 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('glibc','2.17', '55.el7_0.1', 'x86_64', 'updates'),
+        Package('glibc','2.17', '55.el7_0.1', 'x86_64', 'updates'),
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -130,9 +131,9 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.3', '132.el5_8.2', 'x86_64', 'updates'),
-        package_fetcher.Package('xen-libs','3.0.4', '135.el5_8.2', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.3', '132.el5_8.2', 'x86_64', 'updates'),
+        Package('xen-libs','3.0.4', '135.el5_8.2', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -152,8 +153,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.2', '135.el5_8.2', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.2', '135.el5_8.2', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -173,8 +174,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.3', '135.el5_8.3', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.3', '135.el5_8.3', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
@@ -194,8 +195,8 @@ class PackageCheckerTest(unittest.TestCase):
         ])
         pkg = Mock()
         pkg.fetch_installed_packages = Mock(return_value=[
-        package_fetcher.Package('xen-libs','3.0.3', '135.el5_8.1', 'x86_64', 'updates'),
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('xen-libs','3.0.3', '135.el5_8.1', 'x86_64', 'updates'),
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='6')
@@ -213,10 +214,10 @@ class PackageCheckerTest(unittest.TestCase):
         advisory = errata_fetcher.ErrataItem('adv id', ErrataType.SecurityAdvisory,ErrataSeverity.Important, ['x86_64'], ['7'], [{'name': 'xen-libs','version':'3.0.3', 'release':'135.el5_8.2', 'arch':'x86_64'}],[])
         errata.get_errata = Mock(return_value=[advisory])
         pkg = Mock()
-        xen_package = package_fetcher.Package('xen-libs','3.0.3', '135.el5_8.1', 'x86_64', 'updates')
+        xen_package = Package('xen-libs','3.0.3', '135.el5_8.1', 'x86_64', 'updates')
         pkg.fetch_installed_packages = Mock(return_value=[
         xen_package,
-        package_fetcher.Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
+        Package('openssl','2.0', '4.el7', 'x86_64', 'updates')
         ])
         os_fetcher = Mock()
         os_fetcher.get_top_level_version = Mock(return_value='7')
